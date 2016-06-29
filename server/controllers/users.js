c574b7194b400865ef694d3fe5e8eb1c2aeb822d
Todd Enders <tenders64@gmail.com>
2016-06-28 20:20:15 -0700

    initial commit

---
+++ server/controllers/users.js
@@ -0,0 +1,85 @@
+var User = mongoose.model('User');
+
+var sessionUser = {loggedIn: false};
+
+module.exports = (function(){
+  return {
+    index: function(req, res){
+      console.log(sessionUser);
+      User.find({}, function(err, users){
+        if (err){
+          res.json({status: false, errors: "Error retrieving users"});
+        }else{
+          res.json({status: true, users: users});
+        }
+      })
+    },
+    register: function(req, res){
+      User.findOne({email: req.body.email}, function(err, user){
+        if (user) {
+          res.json({status: false, errors: "Email is already in use"});
+        }else{
+          if (req.body.password == req.body.confirm_password){
+            var user = new User({
+              first_name: req.body.first_name,
+              last_name: req.body.last_name,
+              email: req.body.email,
+              password: req.body.password
+            });
+            user.save(function(err){
+              if (err){
+                var errorsArr = [];
+                for (var i in err.errors){
+                  errorsArr.push(err.errors[i].message);
+                }
+                res.json({status: false, errors: errorsArr});
+              }else{
+                sessionUser = {
+                  loggedIn: true,
+                  user_id: user._id,
+                  first_name: user.first_name,
+                  last_name: user.last_name,
+                  email: user.email
+                }
+                res.json({status: true, sessionUser: sessionUser});
+              }
+            })
+          }else{
+            res.json({status: false, errors: "Passwords must match"});
+          }
+        }
+      })
+    },
+    login: function(req, res){
+      if (req.body.email){
+        User.findOne({email: req.body.email}, function(err, user){
+          if(err){
+            res.json({status: false, errors: "Incorrect email or password"});
+          }else{
+            if (user.validPassword(req.body.password)){
+              sessionUser = {
+                loggedIn: true,
+                user_id: user._id,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email: user.email
+              }
+              res.json({status: true, sessionUser: sessionUser});
+            }else{
+              res.json({status: false, errors: "Incorrect email or password"});
+            }
+          }
+        })
+      }else{
+        res.json({status: false, errors: "Must provide Email"});
+      }
+    },
+    session: function(req, res){
+      res.json({status: true, sessionUser: sessionUser});
+    },
+    logout: function(req, res){
+      sessionUser = {loggedIn: false};
+      res.json({status: true, sessionUser: sessionUser})
+    }
+  }
+})();
